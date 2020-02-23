c39df136fa30d53e09a380e384c15967ad945674
John McFarland <mcfarljm@gmail.com>
2020-02-23 11:03:53 -0600

    Add Bitboard.pop_bit and test

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,8 @@ pub const RANK_6: i32 = 5;
 pub const RANK_7: i32 = 6;
 pub const RANK_8: i32 = 7;
 
+const BIT_TABLE: [usize; 64] = [63, 30, 3, 32, 25, 41, 22, 33, 15, 50, 42, 13, 11, 53, 19, 34, 61, 29, 2, 51, 21, 43, 45, 10, 18, 47, 1, 54, 9, 57, 0, 35, 62, 31, 40, 4, 49, 5, 52, 26, 60, 6, 23, 44, 46, 27, 56, 16, 7, 39, 48, 24, 59, 14, 12, 55, 38, 28, 58, 20, 37, 17, 36, 8];
+
 pub fn fr_to_sq(file: i32, rank: i32) -> usize {
     (21 + file + rank * 10) as usize
 }
@@ -56,6 +58,14 @@ impl Bitboard {
             b &= b - 1;
         }
         r
+    }
+
+    pub fn pop_bit(&mut self) -> usize {
+        let b: u64 = self.val ^ (self.val-1);
+        let fold: u32 = ((b & 0xffffffff) ^ (b >> 32)) as u32;
+        self.val &= self.val - 1;
+        let i: usize = (fold.wrapping_mul(0x783a9b23) >> 26) as usize;
+        BIT_TABLE[i]
     }
 
     pub fn to_string(&self) -> String {
@@ -179,5 +189,23 @@ mod tests {
         bb.set_bit(44);
         assert_eq!(bb.count_bits(), 2);
     }
+
+    #[test]
+    fn bb_pop() {
+        let mut bb = Bitboard::new();
+        bb.set_bit(9);
+        bb.set_bit(44);
+        let i = bb.pop_bit();
+        assert_eq!(i, 9);
+        let s = "--------\n\
+                 --------\n\
+                 ----x---\n\
+                 --------\n\
+                 --------\n\
+                 --------\n\
+                 --------\n\
+                 --------\n";
+        assert_eq!(bb.to_string(), s);
+    }  
 
 }
