d214cfa73963786617ff1cce4263281e801dfea6
John McFarland <mcfarljm@gmail.com>
2020-02-24 18:40:07 -0600

    Generate black pawn moves and test

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,9 @@ fn main() {
     // let s = mv.to_string();
     // println!("{}", s);
 
-    let pawn_moves = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1";
-    let board = Board::from_fen(pawn_moves);
+    let pawn_moves_w = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1";
+    let pawn_moves_b = "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1";
+    let board = Board::from_fen(pawn_moves_b);
     println!("{}", board.to_string());
     board.check();
 
--- src/movegen.rs
+++ src/movegen.rs
@@ -47,6 +47,31 @@ impl MoveList {
         }
     }
 
+    // Todo: this could be consolidated with above function.  Only
+    // difference is RANK_2 and BN,BQ
+    fn add_black_pawn_move(&mut self, b: &board::Board, from: usize, to: usize, capture: usize) {
+        // VICE uses two separate functions for capture/no-capture,
+        // but by using a closure, we can handle both cases here.
+        let mut adder: Box<dyn FnMut(usize)>;
+        if capture != Piece::EMPTY {
+            adder = Box::new(|promote: usize| self.add_capture_move(b, moves::Move::new(from, to, capture, promote, false, false)));
+        }
+        else {
+            adder = Box::new(|promote: usize| self.add_quiet_move(b, moves::Move::new(from, to, capture, promote, false, false)));
+        }
+        
+        if board::RANKS[from] == board::RANK_2 {
+            // Add a version of the move with each possible promotion
+            for promote in Piece::BN..=Piece::BQ {
+                adder(promote);
+            }
+        }
+        else {
+            adder(Piece::EMPTY);
+        }
+    }
+
+
     pub fn generate_all_moves(&mut self, b: &board::Board) {
         debug_assert!(b.check());
 
@@ -82,6 +107,36 @@ impl MoveList {
                 }
             }
         }
+        else {
+            for sq in &b.piece_lists[Piece::BP] {
+
+                if b.pieces[sq-10] == Piece::EMPTY {
+                    // pawn forward one square
+                    self.add_black_pawn_move(b, *sq, sq-10, Piece::EMPTY);
+                    if board::RANKS[*sq] == board::RANK_7 && b.pieces[sq-20] == Piece::EMPTY {
+                        // pawn forward two squares
+                        self.add_quiet_move(b, moves::Move::new(*sq, sq-20, Piece::EMPTY, Piece::EMPTY, false, true));
+                    }
+                }
+
+                // Check pawn captures in both directions
+                let dirs: [usize; 2] = [9, 11];
+                for dir in &dirs {
+                    t_sq = sq - dir;
+                    if board::square_on_board(t_sq) && pieces::PIECE_COLOR[b.pieces[t_sq]] == pieces::WHITE {
+                        self.add_black_pawn_move(b, *sq, t_sq, b.pieces[t_sq]);
+                    }
+                }
+
+                // Check en passant captures
+                if sq - 9 == b.en_pas {
+                    self.add_capture_move(b, moves::Move::new(*sq, sq-9, Piece::EMPTY, Piece::EMPTY, true, false));
+                }
+                if sq - 11 == b.en_pas {
+                    self.add_capture_move(b, moves::Move::new(*sq, sq-11, Piece::EMPTY, Piece::EMPTY, true, false));
+                }
+            }
+        }
     }
 
     pub fn print(&self) {
@@ -95,17 +150,27 @@ impl MoveList {
 #[cfg(test)]
 mod tests {
     use crate::movegen::*;
+
+    // Utility function to check move count for a given FEN string
+    fn check_move_count(fen: &str, num_moves: usize) {
+        let board = board::Board::from_fen(fen);
+
+        let mut ml = MoveList::new();
+        ml.generate_all_moves(&board);
+        assert_eq!(ml.moves.len(), num_moves);
+    }
     
     #[test]
     fn white_pawn_start() {
         let pawn_moves_w = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1";
-
-        let board = board::Board::from_fen(pawn_moves_w);
-
-        let mut ml = MoveList::new();
-        ml.generate_all_moves(&board);
         // Todo: move count will need to be updated once moves for
         // other pieces are generated
-        assert_eq!(ml.moves.len(), 26);
+        check_move_count(pawn_moves_w, 26);
+    }
+
+    #[test]
+    fn black_pawn_start() {
+        let pawn_moves_b = "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1";
+        check_move_count(pawn_moves_b, 26);
     }
 }
