43ba99d4dc6df42522880738ff3c1a4dabf6f15a
John McFarland <mcfarljm@gmail.com>
2020-02-26 21:35:38 -0600

    Add Board.clear_piece
    And add helper functions for hashing data in/out

--- src/board/makemove.rs
+++ src/board/makemove.rs
@@ -1,6 +1,46 @@
 use crate::board::*;
 
 impl Board {
+    fn clear_piece(&mut self, sq: Square) {
+        debug_assert!(square_on_board(sq));
+        let piece = self.pieces[sq as usize];
+        debug_assert!(piece.exists());
+
+        let color = piece.color();
+
+        self.hash_piece(piece, sq);
+
+        self.pieces[sq as usize] = Piece::Empty;
+        self.material[color] -= piece.value();
+
+
+        if piece.is_big() {
+            self.num_big_piece[color] -= 1;
+            if piece.is_major() {
+                self.num_major_piece[color] -= 1;
+            } else {
+                self.num_minor_piece[color] -= 1;
+            }
+        } else {
+            let sq64 = SQUARE_120_TO_64[sq as usize];
+            self.pawns[color].clear_bit(sq64);
+            self.pawns[BOTH].clear_bit(sq64);
+        }
+
+        // Remove from piece list
+        let mut i_piece = 0;
+        let mut found = false;
+        for (i, t_sq) in self.piece_lists[piece as usize].iter().enumerate() {
+            if *t_sq == sq {
+                found = true;
+                i_piece = i;
+                break;
+            }
+        }
+        assert!(found);
+        self.piece_lists[piece as usize].swap_remove(i_piece);
+    }
+
     fn hash_piece(&mut self, piece: Piece, sq: Square) {
         self.hash ^= self.hash_keys.piece_keys[piece as usize][sq as usize];
     }
