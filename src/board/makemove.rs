84568f7a0bb0c02fd1a199e19955a381b94ff359
John McFarland <mcfarljm@gmail.com>
2020-02-27 18:59:05 -0600

    Add add_piece and move_piece functions

--- src/board/makemove.rs
+++ src/board/makemove.rs
@@ -41,6 +41,64 @@ impl Board {
         self.piece_lists[piece as usize].swap_remove(i_piece);
     }
 
+    fn add_piece(&mut self, piece: Piece, sq: Square) {
+        debug_assert!(piece.exists());
+        debug_assert!(square_on_board(sq));
+
+        let color = piece.color();
+
+        self.hash_piece(piece, sq);
+        self.pieces[sq as usize] = piece;
+
+        if piece.is_big() {
+            self.num_big_piece[color] += 1;
+            if piece.is_major() {
+                self.num_major_piece[color] += 1;
+            } else {
+                self.num_minor_piece[color] += 1;
+            }
+        } else {
+            let sq64 = SQUARE_120_TO_64[sq as usize];
+            self.pawns[color].set_bit(sq64);
+            self.pawns[BOTH].set_bit(sq64);
+        }
+
+        self.material[color] += piece.value();
+        self.piece_lists[piece as usize].push(sq);
+    }
+
+    fn move_piece(&mut self, from: Square, to: Square) {
+        debug_assert!(square_on_board(from));
+        debug_assert!(square_on_board(to));
+        let piece = self.pieces[from as usize];
+        let color = piece.color();
+
+        self.hash_piece(piece, from);
+        self.pieces[from as usize] = Piece::Empty;
+
+        self.hash_piece(piece, to);
+        self.pieces[to as usize] = piece;
+
+        if ! piece.is_big() {
+            let from64 = SQUARE_120_TO_64[from as usize];
+            let to64 = SQUARE_120_TO_64[to as usize];
+            self.pawns[color].clear_bit(from64);
+            self.pawns[BOTH].clear_bit(from64);
+            self.pawns[color].set_bit(to64);
+            self.pawns[BOTH].set_bit(to64);
+        }
+
+        let mut found = false;
+        for (i, t_sq) in self.piece_lists[piece as usize].iter().enumerate() {
+            if *t_sq == from {
+                found = true;
+                self.piece_lists[piece as usize][i] = to;
+                break;
+            }
+        }
+        assert!(found);
+    }
+
     fn hash_piece(&mut self, piece: Piece, sq: Square) {
         self.hash ^= self.hash_keys.piece_keys[piece as usize][sq as usize];
     }
