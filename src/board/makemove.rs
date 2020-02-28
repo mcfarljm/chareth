97e0690a0b10d8c668e6568b9f0d7b2961ebfd18
John McFarland <mcfarljm@gmail.com>
2020-02-27 20:44:44 -0600

    Add Board.undo_move

--- src/board/makemove.rs
+++ src/board/makemove.rs
@@ -109,12 +109,85 @@ impl Board {
         debug_assert!(self.check());
 
         if self.square_attacked(self.king_sq[self.side], self.side) {
-            // Todo: uncomment
-            // self.undo_move();
+            self.undo_move();
             return false;
         }
         
         true
+    }
+
+    fn undo_move(&mut self) {
+        debug_assert!(self.check());
+
+        self.hist_ply -= 1;
+        self.ply -= 1;
+
+        let undo = self.history.pop().unwrap();
+        let mv = &undo.mv;
+        let from = mv.from();
+        let to = mv.to();
+
+        debug_assert!(square_on_board(from));
+        debug_assert!(square_on_board(to));
+
+        if self.en_pas != Position::NONE as Square {
+            self.hash_en_pas();
+        }
+        self.hash_castle();
+
+        self.castle_perm = undo.castle_perm;
+        self.fifty_move = undo.fifty_move;
+        self.en_pas = undo.en_pas;
+
+        if self.en_pas != Position::NONE as Square {
+            self.hash_en_pas();
+        }
+        self.hash_castle();
+
+        self.side ^= 1;
+        self.hash_side();
+
+        if mv.is_en_pas() {
+            if self.side == WHITE {
+                self.add_piece(Piece::BP, to-10);
+            } else {
+                self.add_piece(Piece::WP, to+10);
+            }
+        } else if mv.is_castle() {
+            if to == Position::C1 as Square {
+                self.move_piece(Position::D1 as Square, Position::A1 as Square);
+            }
+            else if to == Position::C8 as Square {
+                self.move_piece(Position::D8 as Square, Position::A8 as Square);
+            }
+            else if to == Position::G1 as Square {
+                self.move_piece(Position::F1 as Square, Position::H1 as Square);
+            }
+            else if to == Position::G8 as Square {
+                self.move_piece(Position::F8 as Square, Position::H8 as Square);
+            }
+            else {
+                panic!("invalid to position in undo move with castle flag");
+            }
+        }
+
+        self.move_piece(to, from);
+
+        if self.pieces[from as usize].is_king() {
+            self.king_sq[self.side] = from;
+        }
+
+        if mv.is_capture() {
+            self.add_piece(mv.capture, to);
+        }
+
+        if mv.is_promotion() {
+            debug_assert!(mv.promote.exists() && ! mv.promote.is_pawn());
+            self.clear_piece(from);
+            self.add_piece(if mv.promote.color() == WHITE { Piece::WP } else { Piece::BP }, from)
+        }
+
+        debug_assert!(self.check());
     }
     
     fn clear_piece(&mut self, sq: Square) {
